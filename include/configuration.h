18b25de4a39e635cfd6c150c32d17ca9e6ab0df2
Emil Varga <varga.emil@gmail.com>
2018-05-18 14:59:57 +0200

    GNU GPLv3

--- include/configuration.h
+++ include/configuration.h
@@ -1,10 +1,22 @@
-/*
- * configuration.h
- *
- *  Created on: Mar 8, 2018
- *      Author: emil
- *
- *
+/*******************************************************************************
+ * Copyright (C) 2018 Emil Varga <varga.emil@gmail.com>
+ * 
+ * This file is part of OpenVort
+ * 
+ * OpenVort is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************
+
  *  The configuration support is handled through libconfig.
  *
  *  Functions declared here load the configuration file, modify the

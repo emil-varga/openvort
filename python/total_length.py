cf86fc8e240c6cdcbdaa498e888294fbb7171e23
Emil Varga <varga.emil@gmail.com>
2019-02-25 21:59:36 +0100

    plotting in total_length.py

--- python/total_length.py
+++ python/total_length.py
@@ -32,21 +32,21 @@ if __name__ == '__main__':
         print('provide data dir as the command line argument')
     data_dir = path.abspath(sys.argv[1])
     cfg_filename = glob(path.join(data_dir, '*.cfg'))[0]
-    
+
     with open(path.join(data_dir, cfg_filename)) as cfg_file:
         config = libconf.load(cfg_file);
-        
+
     dt0 = config['dt']
     frame_shots = config['frame_shots']
     dt = frame_shots * dt0
-    
+
     files = glob(path.join(data_dir, 'frame*.dat'))
     files.sort()
-    
+
     time = dt
     times = []
     lengths = []
-    
+
     for fn in files:
         print(fn)
         d = np.loadtxt(fn)
@@ -56,7 +56,7 @@ if __name__ == '__main__':
             ix = d[:,0] == vidx;
             if not any(ix):
                 break
-    
+
             rs = d[ix, 1:4]
             dr = np.diff(rs, axis=0)
             l += np.sum(np.sqrt(dr**2))
@@ -64,6 +64,12 @@ if __name__ == '__main__':
         lengths.append(l)
         times.append(time)
         time += dt
-    
+
+    f, ax = plt.subplots(1,1)
+    ax.plot(times, lengths)
+    ax.set_xlabel('time (s)')
+    ax.set_ylabel('total length (cm)')
+    plt.show()
+
     out = np.column_stack((times, lengths))
     np.savetxt(path.join(data_dir, 'L_t.txt'), out, header = 'time(s)\ttotal length(cm)')
